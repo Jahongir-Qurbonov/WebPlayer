f5d4c1277cb6ab00cb3b58191baa62b4e85efef7
Jahongir Qurbonov <qurbonovjahongir17@gmail.com>
2022-09-26 21:03:20 +0500

    add player app to INSTALLED_APPS in settings

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local apps
+    'player.apps.PlayerConfig',
+    
 ]
 
 MIDDLEWARE = [
