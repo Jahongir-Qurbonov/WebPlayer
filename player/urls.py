6f7d94d129ef5746d1cd531a1026f3e74868c6cb
Jahongir Qurbonov <qurbonovjahongir17@gmail.com>
2022-09-26 22:07:29 +0500

    song player path add

--- player/urls.py
+++ player/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
+from .views import songs_player
 
 urlpatterns = [
-    
+    path('', songs_player, name='player'),
 ]
--- player/views.py
+++ player/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from .models import Song
 
 # Create your views here.
+
+def songs_player(request: HttpRequest):
+    songs = Song.objects.all()
+    context_songs = []
+    for i, song in enumerate(songs, start=1):
+        context_songs.append({
+            'id': i,
+            'title': song.title,
+            'artist': song.artist,
+            'album': song.album,
+            'genre': song.genre,
+            'audio_file': song.audio_file[:song.audio_file.rfind('.')],
+        })
+
+    context = {
+        'songs': context_songs,
+    }
+
+    return render(request, template_name='player/songs_player.html', context=context)
