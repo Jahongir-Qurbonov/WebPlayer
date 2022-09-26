31dc475a3d831a3bc3f1b34072c2381550a95886
Jahongir Qurbonov <qurbonovjahongir17@gmail.com>
2022-09-26 21:11:28 +0500

    create song model

--- player/models.py
+++ player/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    artist = models.CharField(max_length=100, blank=True, null=True)
+    album = models.CharField(max_length=100, blank=True, null=True)
+    genre = models.CharField(max_length=100, blank=True, null=True)
+    audio_file = models.FileField(upload_to='audio_files/')
+
+    def __str__(self):
+        return self.title
