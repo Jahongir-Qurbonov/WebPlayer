fe08141733c2a3f3f664994b2f9ab2ca78357742
Jahongir Qurbonov <qurbonovjahongir17@gmail.com>
2022-09-27 21:23:00 +0500

    signals fix

--- player/apps.py
+++ player/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PlayerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'player'
+
+    def ready(self) -> None:
+        from . import signals
+        return super().ready()
