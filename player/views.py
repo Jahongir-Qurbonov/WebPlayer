9811b35ee67dc790de054caf81f8c55bc02eed1e
Jahongir Qurbonov <qurbonovjahongir17@gmail.com>
2022-09-27 21:25:51 +0500

    fixed audio file name

--- player/views.py
+++ player/views.py
@@ -8,13 +8,14 @@ def songs_player(request: HttpRequest):
     songs = Song.objects.all()
     context_songs = []
     for i, song in enumerate(songs, start=1):
+        print(song.audio_file)
         context_songs.append({
             'id': i,
             'title': song.title,
             'artist': song.artist,
             'album': song.album,
             'genre': song.genre,
-            'audio_file': song.audio_file[:song.audio_file.rfind('.')],
+            'audio_file': song.audio_file.name[:str(song.audio_file).rfind('.')],
         })
 
     context = {
